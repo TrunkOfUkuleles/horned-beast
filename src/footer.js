b19aae48ba5c1215eabe373c9feaa909f184e8e2
TrunkOfUkuleles <60909020+TrunkOfUkuleles@users.noreply.github.com>
2021-03-16 16:05:56 -0700

    fixing headers

--- src/footer.js
+++ src/footer.js
@@ -12,7 +12,7 @@ class Footer extends React.Component {
     }
     render (){
       let handleClick = () => {
-        this.setState({bgColor:blue})
+        this.setState({bgColor:'blue'})
       }
 
      return (
--- src/header.js
+++ src/header.js
@@ -4,9 +4,20 @@ import React from 'react';
 
 class Header extends React.Component{
 
+    constructor(props) {
+        super(props);
+        this.state = {
+          bgColor: ""
+        }
+      }
+
     render() { 
+        let handleClick = () => {
+            this.setState({bgColor:'blue'})
+          }
+
         return (
-        <h1>Them Horned Boys</h1>
+        <h1 style={{backgroundColor: this.state.bgColor}} onClick={handleClick}>Them Horned Boys</h1>
     )}
 }
 
